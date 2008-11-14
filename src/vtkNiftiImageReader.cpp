f1c5bc2700275e9753d26655bdb0e4ad3ee0014a
Daniele E. Domenichelli <daniele.domenichelli@unige.it>
2008-11-14 18:13:47 +0000

    ENH: vtkNiftiImageReader reads nii.gz files without using gunzip
    git-svn-id: http://dwarf05.bio.dist.unige.it/svn/taos/src/viewer/trunk@65 3f367682-104b-0410-a550-fdff993939f5

--- src/vtkNiftiImageReader.cpp
+++ src/vtkNiftiImageReader.cpp
@@ -4,6 +4,9 @@
 #include "vtkNiftiImageReader.h"
 
 #include <vtkObjectFactory.h>
+#include <vtkImageData.h>
+#include <vtkPointData.h>
+#include <vtkDataArray.h>
 
 #include <nifti1_io.h>
 #include <string>
@@ -13,11 +16,10 @@
 #define MSB_FIRST 2
 
 
+//#define DEBUG_ALL
 
 
-
-
-vtkCxxRevisionMacro(vtkNiftiImageReader, "$Revision: 1.9 $");
+vtkCxxRevisionMacro(vtkNiftiImageReader, "$Revision: 2.0 $");
 vtkStandardNewMacro(vtkNiftiImageReader);
 
 vtkNiftiImageReader::vtkNiftiImageReader()
@@ -28,210 +30,246 @@ vtkNiftiImageReader::~vtkNiftiImageReader()
 {
 }
 
-
 void vtkNiftiImageReader::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os,indent);
 }
 
-void vtkNiftiImageReader::SetFileName(const char* fn)
+int vtkNiftiImageReader::CanReadFile(const char* fname)
 {
-    nifti_image *nim;
-
-
-    //Controlla che l'header sia valido
-    if (is_nifti_file(fn) < 0)
-        std::cerr << "ERROR: void vtkNiftiImageReader::SetFileName(const char* fn) -> is_nifti_file(fn) < 0" << std::endl;
-
-    //Controlla se il file è gzippato
-    //TODO supportare anche i nifti compressi senza scompattarli
-    if (nifti_is_gzfile(fn))
-    {
-        std::cerr << "WARNING: void vtkNiftiImageReader::SetFileName(const char* fn) -> nifti_is_gzfile(fn)" << std::endl;
-        std::string command("gunzip -f -c ");
-        command += fn;
-        command += " > /tmp/tmp.nii";
-        std::cout << command << std::endl;
-        system(command.c_str());
-        nim = nifti_image_read("/tmp/tmp.nii", 0); //0 legge solo l'header 1 legge anche i dati
-    }
-    else
-    {
-        nim = nifti_image_read(fn, 0); //0 legge solo l'header 1 legge anche i dati
-    }
-//    nifti_image_infodump(nim);
-
-    //Controlla che il numero delle dimensioni sia 3
-    //TODO supportare anche un numero diverso di dimensioni
-    if (nim->ndim != 3 && !(nim->ndim == 4 && nim->nt == 1))
-        std::cerr << "WARNING: void vtkNiftiImageReader::SetFileName(const char* fn) -> nim->ndim != 3 && !(nim->ndim == 4 && nim->nt == 1)" << std::endl;
-    this->SetFileDimensionality(3);
-
-/*
-    CONVENZIONE RADIOLOGICA - NEUROLOGICA
-    TODO Informarsi meglio a riguardo
-
-    NON FUNZIONA CON vtkImageReader2 (con vtkImageReader funziona!)
-
-    //Controlla le direzioni del file (convenzione radiologica/neurologica?)
-    //TODO Controllare se è giusto!
-    int i,j,k;
-    nifti_mat44_to_orientation( nim->sto_xyz , &i,&j,&k ) ;
-    if( i <= 0 || j <= 0 || k <= 0 )
-        return false;
-    double dir[3] = {1,1,1};
-    if(i==NIFTI_L2R)    //Left-to-Right
-        dir[0]=-1;
-    else if(i==NIFTI_R2L)   //Right-to-Left
-        dir[0]=1;
-    if(j==NIFTI_P2A)    //Posterior-to-Anterior
-        dir[1]=1;
-    else if(j==NIFTI_A2P)   //Anterior-to-Posterior
-        dir[1]=-1;
-    if(k==NIFTI_I2S)    //Inferior-to-Superior
-        dir[2]=1;
-    if(k==NIFTI_S2I)    //Superior-to-Inferior
-        dir[2]=-1;
-    vtkTransform *orientation = vtkTransform::New(); //TODO si può fare in un altro modo?
-    orientation->Scale(dir);
-    this->SetTransform(orientation); //TODO controllare se si possono poi impostare più trasformazioni
-    orientation->Delete();
-*/
-/*
-for(int i=0;i<4;i++)
-{
-    for (int j=0; j<4; j++)
-        std::cerr << nim->sto_xyz.m[i][j] << "\t";
-    std::cerr << std::endl;
+  if (is_nifti_file(fname) < 0)
+    return 0;
+  return 3;
 }
-*/
 
-    nifti_mat44_to_orientation( nim->sto_xyz , &m_Orientation[0],&m_Orientation[1],&m_Orientation[2] ) ;
 
-#ifdef DEBUG
-    std::cout << "Orientation = " << std::flush;
-    for (int i=0; i<3; i++)
-    {
-        switch (m_Orientation[i])
-        {
-            case NIFTI_L2R:
-                std::cout << "R" << std::flush;
-                break;
-            case NIFTI_R2L:
-                std::cout << "L" << std::flush;
-                break;
-            case NIFTI_P2A:
-                std::cout << "A" << std::flush;
-                break;
-            case NIFTI_A2P:
-                std::cout << "P" << std::flush;
-                break;
-            case NIFTI_I2S:
-                std::cout << "S" << std::flush;
-                break;
-            case NIFTI_S2I:
-                std::cout << "I" << std::flush;
-                break;
-            default:
-                std::cout << "?" << std::flush;
-                break;
-        }
-    }
-    std::cout << std::endl;
-#endif //DEBUG
 
-    //Imposta FileLowerLeft
-    this->SetFileLowerLeft(true);
 
-    //Imposta l'offset nel file
-    this->SetHeaderSize(nim->iname_offset); // size of the header
+void vtkNiftiImageReader::ExecuteInformation()
+{
+  this->ComputeInternalFileName(this->DataExtent[4]);
+  if (this->InternalFileName == NULL)
+  {
+    return;
+  }
 
-    //Imposta l'immagine in scala di grigi
+  nifti_image *nim;
 
-    //Imposta posizione, voxel spacing e origine
-    this->SetDataExtent(0,(nim->nx)-1, 0,(nim->ny)-1, 0,(nim->nz)-1);
-    this->SetDataSpacing(nim->dx, nim->dy, nim->dz);       // voxel spacing 
-    this->SetDataOrigin(nim->dx/2.0, nim->dy/2.0, nim->dy/2.0); //TODO Probabilmente è meglio impostare l'origine al centro
-    //this->SetDataOrigin(0.0, 0.0, 0.0);
-    //Imposta il numero di byte per voxel
-    switch (nim->datatype)
-    {
-        //case DT_UNSIGNED_CHAR:                          // unsigned char (8 bits/voxel)
-        case DT_UINT8:                                  // unsigned char (8 bits/voxel)
-            this->SetDataScalarTypeToUnsignedChar();
-            this->SetNumberOfScalarComponents(1);
-            break;
-        //case DT_SIGNED_SHORT:                           // signed short (16 bits/voxel)
-        case DT_INT16:                                  // signed short (16 bits/voxel)
-            this->SetDataScalarTypeToShort();
-            this->SetNumberOfScalarComponents(1);
-            break;
-        case DT_UINT16:                                 // unsigned short (16 bits)
-            this->SetDataScalarTypeToUnsignedShort();
-            this->SetNumberOfScalarComponents(1);
-            break;
-        //case DT_RGB:                                    // RGB triple (24 bits/voxel)
-        case DT_RGB24:                                  // RGB triple (24 bits/voxel)
-            std::cerr << "WARNING: dataset = DT_RGB non testato" << std::endl;
-            this->SetDataScalarTypeToUnsignedChar();
-            this->SetNumberOfScalarComponents(3);
-            break;
-        //case DT_SIGNED_INT:                             // signed int (32 bits/voxel)
-        case DT_INT32:                                  // signed int (32 bits/voxel)
-            this->SetDataScalarTypeToInt();
-            this->SetNumberOfScalarComponents(1);
-            break;
-        //case DT_FLOAT:                                  // float (32 bits/voxel)
-        case DT_FLOAT32:                                // float (32 bits/voxel)
-            this->SetDataScalarTypeToFloat();
-            this->SetNumberOfScalarComponents(1);
-            break;
-        //case DT_DOUBLE:                                 // double (64 bits/voxel)
-        case DT_FLOAT64:                                // double (64 bits/voxel)
-            this->SetDataScalarTypeToDouble();
-            this->SetNumberOfScalarComponents(1);
-            break;
-        case DT_ALL:
-        case DT_BINARY:                                 // binary (1 bit/voxel)
-        case DT_INT8:                                   // signed char (8 bits)
-        case DT_UINT32:                                 // unsigned int (32 bits)
-        //case DT_COMPLEX:                                // complex (64 bits/voxel)
-        case DT_COMPLEX64:                              // complex (64 bits/voxel)
-        case DT_INT64:                                  // long long (64 bits)
-        case DT_UINT64:                                 // unsigned long long (64 bits)
-        case DT_FLOAT128:                               // long double (128 bits)
-        case DT_COMPLEX128:                             // double pair (128 bits)
-        case DT_COMPLEX256:                             // long double pair (256 bits)
-            std::cerr << "ERROR: dataset non supportato dalle VTK" << std::endl;
-            break;
-        //case DT_NONE:
-        case DT_UNKNOWN:
-        default:
-            std::cerr << "ERROR: dataset unknown" << std::endl;
-            break;
+#ifdef DEBUG_ALL
+  nifti_image_infodump(nim);
+#endif //DEBUG_ALL
+
+
+  nim = nifti_image_read(this->GetInternalFileName(), 0); //0 legge solo l'header 1 legge anche i dati
+
+  if (!nim)
+  {
+    vtkErrorWithObjectMacro(this,
+                            "niftilib could not read file: "
+                            << this->InternalFileName);
+    return;
+  }
+
+  //TODO support images of all dimensions
+  if (nim->ndim != 3 && !(nim->ndim == 4 && nim->nt == 1))
+  {
+    vtkErrorWithObjectMacro(this,
+                            "Supported only 3D images: "
+                            << this->InternalFileName);
+    return;
+  }
+
+//Set dimension, position, voxel spacing and origin
+
+  this->SetFileDimensionality(3);
+  this->SetDataExtent(0,(nim->nx)-1, 0,(nim->ny)-1, 0,(nim->nz)-1);
+  this->SetDataSpacing(nim->dx, nim->dy, nim->dz);       // voxel spacing 
+  //TODO Check if origin should be in the center of voxel or in [0,0,0] ( this->SetDataOrigin(0.0, 0.0, 0.0); )
+  //TODO Check if position in space could be found in nifti header
+  this->SetDataOrigin(nim->dx/2.0, nim->dy/2.0, nim->dy/2.0); // Set origin in the center of the first voxel
+
+//Set datatype
+  switch (nim->datatype)
+  {
+    //case DT_UNSIGNED_CHAR:                          // unsigned char (8 bits/voxel)
+    case DT_UINT8:                                  // unsigned char (8 bits/voxel)
+      this->SetDataScalarTypeToUnsignedChar();
+      this->SetNumberOfScalarComponents(1);
+      break;
+    //case DT_SIGNED_SHORT:                           // signed short (16 bits/voxel)
+    case DT_INT16:                                  // signed short (16 bits/voxel)
+      this->SetDataScalarTypeToShort();
+      this->SetNumberOfScalarComponents(1);
+      break;
+    case DT_UINT16:                                 // unsigned short (16 bits)
+      this->SetDataScalarTypeToUnsignedShort();
+      this->SetNumberOfScalarComponents(1);
+      break;
+    //case DT_RGB:                                    // RGB triple (24 bits/voxel)
+    case DT_RGB24:                                  // RGB triple (24 bits/voxel)
+      vtkWarningWithObjectMacro( this,
+                                 "dataset = DT_RGB not tested yet: "
+                                 << this->InternalFileName );
+      this->SetDataScalarTypeToUnsignedChar();
+      this->SetNumberOfScalarComponents(3);
+      break;
+    //case DT_SIGNED_INT:                             // signed int (32 bits/voxel)
+    case DT_INT32:                                  // signed int (32 bits/voxel)
+      this->SetDataScalarTypeToInt();
+      this->SetNumberOfScalarComponents(1);
+      break;
+    //case DT_FLOAT:                                  // float (32 bits/voxel)
+    case DT_FLOAT32:                                // float (32 bits/voxel)
+      this->SetDataScalarTypeToFloat();
+      this->SetNumberOfScalarComponents(1);
+      break;
+    //case DT_DOUBLE:                                 // double (64 bits/voxel)
+    case DT_FLOAT64:                                // double (64 bits/voxel)
+      this->SetDataScalarTypeToDouble();
+      this->SetNumberOfScalarComponents(1);
+      break;
+    case DT_ALL:
+    case DT_BINARY:                                 // binary (1 bit/voxel)
+    case DT_INT8:                                   // signed char (8 bits)
+    case DT_UINT32:                                 // unsigned int (32 bits)
+    //case DT_COMPLEX:                                // complex (64 bits/voxel)
+    case DT_COMPLEX64:                              // complex (64 bits/voxel)
+    case DT_INT64:                                  // long long (64 bits)
+    case DT_UINT64:                                 // unsigned long long (64 bits)
+    case DT_FLOAT128:                               // long double (128 bits)
+    case DT_COMPLEX128:                             // double pair (128 bits)
+    case DT_COMPLEX256:                             // long double pair (256 bits)
+      vtkErrorWithObjectMacro( this,
+                               "datatype not supported: "
+                               << this->InternalFileName );
+      return;
+    //case DT_NONE:
+    case DT_UNKNOWN:
+    default:
+      vtkErrorWithObjectMacro( this,
+                               "datatype unknown: "
+                               << this->InternalFileName );
+      return;
     } //switch (nim->datatype)
 
 
-    // Controlla se deve invertire l'ordine dei bytes
-    //    BigEndian => most significant byte first
-    //    LittleEndian => less significant byte first
-    switch (nim->byteorder)
+// Check if bytes should be swapped
+//    BigEndian => most significant byte first
+//    LittleEndian => less significant byte first
+  switch (nim->byteorder)
+  {
+    case MSB_FIRST:
+      this->SetDataByteOrderToBigEndian();
+      break;
+    case LSB_FIRST:
+      this->SetDataByteOrderToLittleEndian();
+      break;
+    default:
+      vtkWarningWithObjectMacro( this,
+                                 "unknown data byte order: "
+                                 << this->InternalFileName);
+      break;
+  }
+
+
+//TODO Tutto quello che c'è qua sotto dovrà un giorno essere eliminato
+//BEGIN CUT
+  nifti_mat44_to_orientation( nim->sto_xyz , &m_Orientation[0],&m_Orientation[1],&m_Orientation[2] ) ;
+
+#ifdef DEBUG
+  std::cout << "Orientation = " << std::flush;
+  for (int i=0; i<3; i++)
+  {
+    switch (m_Orientation[i])
     {
-        case MSB_FIRST:
-            this->SetDataByteOrderToBigEndian();
-            break;
-        case LSB_FIRST:
-            this->SetDataByteOrderToLittleEndian();
-            break;
-        default:
-            std::cerr << "WARNING: unknown data byte order" << std::endl;
+      case NIFTI_L2R:
+        std::cout << "R" << std::flush;
+        break;
+      case NIFTI_R2L:
+        std::cout << "L" << std::flush;
+        break;
+      case NIFTI_P2A:
+        std::cout << "A" << std::flush;
+        break;
+      case NIFTI_A2P:
+        std::cout << "P" << std::flush;
+        break;
+      case NIFTI_I2S:
+        std::cout << "S" << std::flush;
+        break;
+      case NIFTI_S2I:
+        std::cout << "I" << std::flush;
+        break;
+      default:
+        std::cout << "?" << std::flush;
+        break;
     }
+  }
+  std::cout << std::endl;
+#endif //DEBUG
+//END CUT
 
 
-    //Imposta il nome del file da leggere (sceglie in modo automatico sia che il file sia unico sia che siano 2)
-    this->vtkImageReader2::SetFileName(nim->iname);
-    nifti_image_free(nim);
+  this->vtkImageReader2::ExecuteInformation();
+
+  //Free memory
+  nifti_image_free(nim);
+
+
 }
+
+
+
+//----------------------------------------------------------------------------
+// This function reads a data from a file.  The datas extent/axes
+// are assumed to be the same as the file extent/order.
+void vtkNiftiImageReader::ExecuteData(vtkDataObject *output)
+{
+  vtkImageData *data = this->AllocateOutputData(output);
+
+  if (this->InternalFileName == NULL)
+  {
+    vtkErrorMacro("A FileName must be specified.");
+    return;
+  }
+
+  this->ComputeDataIncrements();
+  data->GetPointData()->GetScalars()->SetName("NIFTI1Image");
+
+  void* outPtr = data->GetScalarPointer();
+
+  nifti_image *nim;
+
+  nim = nifti_image_read(this->GetInternalFileName(), 1); //0 legge solo l'header 1 legge anche i dati
+
+  if (!nim)
+  {
+    vtkErrorWithObjectMacro(this,
+                            "niftilib could not read file: "
+                            << this->InternalFileName);
+    return;
+  }
+
+  if( (data->GetNumberOfPoints() * data->GetScalarSize()) != (nim->nbyper * nim->nvox))
+  {
+    vtkWarningWithObjectMacro(this,
+                              "(data->GetNumberOfPoints() * data->GetScalarSize()) != (nim->nbyper * nim->nvox): "
+                              << this->InternalFileName);
+  }
+
+  //void * memcpy ( void * destination, const void * source, size_t num );
+  memcpy (outPtr, nim->data, nim->nbyper*nim->nvox);
+
+  //Free memory
+  nifti_image_free(nim);
+}
+
+
+
+
+
+
+
 
 
 
--- src/vtkNiftiImageReader.h
+++ src/vtkNiftiImageReader.h
@@ -13,26 +13,54 @@
 class VTK_IO_EXPORT vtkNiftiImageReader : public vtkImageReader2
 {
 public:
-    static vtkNiftiImageReader *New();
-    vtkTypeRevisionMacro(vtkNiftiImageReader,vtkImageReader2);
-    void PrintSelf(ostream& os, vtkIndent indent);
+  static vtkNiftiImageReader *New();
+  vtkTypeRevisionMacro(vtkNiftiImageReader,vtkImageReader2);
+  virtual void PrintSelf(ostream& os, vtkIndent indent);
 
-    inline int CanReadFile(const char* fname) {return 3;} /// \todo controllare se effettivamente può leggere il file
+  /*!
+   * \brief Is the given file a NIFTI1 file?
+   */
+  int CanReadFile(const char* fname);
 
-    void SetFileName(const char* fn);
+  /*! Get the file extensions for this format.
+   * \brief Returns a string with a space separated list of extensions in the format .extension
+   */
+  virtual const char* GetFileExtensions()
+  {
+    return ".nii .hdr .nii.gz";
+  }
 
+  /*!
+   * Return a descriptive name for the file format that might be useful in a GUI.
+   */
+  virtual const char* GetDescriptiveName()
+    {
+      return "NIFTI1";
+    }
+
+protected:
+  vtkNiftiImageReader();
+  ~vtkNiftiImageReader();
+
+  virtual void ExecuteInformation();
+  virtual void ExecuteData(vtkDataObject *out);
+
+private:
+  vtkNiftiImageReader(const vtkNiftiImageReader&);  // Not implemented.
+  void operator=(const vtkNiftiImageReader&);  // Not implemented.
+
+
+//TODO Tutto quello che c'è qua sotto dovrà un giorno essere eliminato
+//BEGIN CUT
+public:
+//    void SetFileName(const char* fn);
     inline void GetOrientation(int& orient0, int& orient1, int& orient2) {orient0 = m_Orientation[0];
                                                                           orient1 = m_Orientation[1];
                                                                           orient2 = m_Orientation[2];}
-
-protected:
-    vtkNiftiImageReader();
-    ~vtkNiftiImageReader();
-
-
 private:
-
     int m_Orientation[3];
+//END CUT
+
 }; // class vtkNiftiImageReader
 
 
